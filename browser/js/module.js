7bf50b9b1242225fba0e5743d0295ba2dc424111
Sahil Ishar <sahil.ishar@gmail.com>
2016-04-02 10:11:55 -0400

    Added default child behavior

--- browser/js/module.js
+++ browser/js/module.js
@@ -1,3 +1,10 @@
 'use strict';
 
 var juke = angular.module('juke', ['ui.router']);
+
+juke.config(function($urlRouterProvider){
+    // when there is an empty route, redirect to /index   
+    $urlRouterProvider.when('', '/albums');
+
+    $urlRouterProvider.when('/artists/:id', '/artists/:id/albums');
+});
