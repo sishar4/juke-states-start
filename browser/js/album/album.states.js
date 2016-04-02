74f918268397c02c498bbba7e66796ccf462af95
Sahil Ishar <sahil.ishar@gmail.com>
2016-04-01 20:38:15 -0400

    Using templateUrl to pass in html files in views

--- browser/js/album/album.states.js
+++ browser/js/album/album.states.js
@@ -3,23 +3,7 @@ juke
 .config(function ($stateProvider) {
   $stateProvider.state('albumList', {
     url: '/albums',
-    template: '' +
-      '<div ng-show="showMe">' +
-        '<h3>Albums</h3>' +
-        '<div class="row">' +
-          '<div ng-repeat="album in albums" class="col-xs-4">' +
-            '<a class="thumbnail" ui-sref="listOneAlbum({id: album._id})" >' +
-                '<img ng-src="{{ album.imageUrl }}">' +
-                '<div class="caption">' +
-                  '<h5>' +
-                    '<span>{{ album.name }}</span>' +
-                  '</h5>' +
-                  '<small>{{ album.songs.length }} songs</small>' +
-                '</div>' +
-              '</a>' +
-          '</div>' +
-        '</div>' +
-      '</div>',
+    templateUrl: './allAlbums.html',
       controller: 'AlbumsCtrl'
   });
 });
@@ -27,35 +11,7 @@ juke
 juke.config(function ($stateProvider) {
   $stateProvider.state('listOneAlbum', {
     url: '/:id',
-    template: '' +
-      '<div class="album" ng-show="showMe">' +
-          '<p>' +
-            '<h3>{{ album.name }}</h3>' +
-            '<img ng-src="{{ album.imageUrl }}" class="img-thumbnail">' +
-          '</p>' +
-          '<table class="table">' +
-            '<thead>' +
-              '<tr>' +
-                '<th></th>' +
-                '<th>Name</th>' +
-                '<th>Artists</th>' +
-                '<th>Genres</th>' +
-              '</tr>' +
-            '</thead>' +
-            '<tbody>' +
-              '<tr ng-repeat="song in album.songs" ng-class="{active: song === getCurrentSong() }">' +
-                '<td>' +
-                  '<button class="btn btn-default btn-xs" ng-click="toggle(song)">' +
-                    '<span class="glyphicon" ng-class="{ \'glyphicon-pause\': isPlaying(song), \'glyphicon-play\': !isPlaying(song) }"></span>' +
-                  '</button>' +
-                '</td>' +
-                '<td>{{ song.name }}</td>' +
-                '<td><span ng-repeat="artist in song.artists">{{ artist.name }}{{ $last ? "" : ", " }}</span></td>' +
-                '<td>{{ song.genres.join(", ") }}</td>' +
-              '</tr>' +
-            '</tbody>' +
-          '</table>' +
-        '</div>',
+    templateUrl: './oneAlbum.html',
     controller: 'AlbumCtrl'
   });
 });
--- browser/js/artist/artist.states.js
+++ browser/js/artist/artist.states.js
@@ -3,15 +3,7 @@
 juke.config(function ($stateProvider) {
   $stateProvider.state('artistList', {
     url: '/albums/artists',
-    template: '' +
-      '<div>' +
-          '<h3>Artists</h3>' +
-            '<div class="list-group">' +
-              '<div class="list-group-item" ng-repeat="artist in artists">' +
-              '<a ui-sref="listOneArtist({id: artist._id})">{{ artist.name }}</a>' +
-            '</div>' +
-          '</div>' +
-        '</div>',
+    templateUrl: './allArtists.html',
       controller: 'ArtistsCtrl'
   });
 });
@@ -19,46 +11,7 @@ juke.config(function ($stateProvider) {
 juke.config(function ($stateProvider) {
   $stateProvider.state('listOneArtist', {
     url: '/artists/:id',
-    template: '' +
-      '<div>' +
-            '<h3>{{ artist.name }}</h3>' +
-            '<h3>Albums</h3>' +
-            '<div class="row">' +
-              '<div class="col-xs-4" ng-repeat="album in artist.albums">' +
-                '<a class="thumbnail" href="#" ng-click="viewOneAlbum(album)">' +
-                  '<img ng-src="{{ album.imageUrl }}">' +
-                  '<div class="caption">' +
-                    '<h5>' +
-                      '<span>{{ album.name }}</span>' +
-                    '</h5>' +
-                    '<small>{{ album.songs.length }} songs</small>' +
-                  '</div>' +
-                '</a>' +
-              '</div>' +
-            '</div>' +
-            '<table class="table">' +
-              '<thead>' +
-                '<tr>' +
-                  '<th></th>' +
-                  '<th>Name</th>' +
-                  '<th>Artists</th>' +
-                  '<th>Genres</th>' +
-                '</tr>' +
-              '</thead>' +
-              '<tbody>' +
-                '<tr ng-repeat="song in artist.songs" ng-class="{active: song === getCurrentSong() }">' +
-                  '<td>' +
-                    '<button class="btn btn-default btn-xs" ng-click="toggle(song)">' +
-                      '<span class="glyphicon" ng-class="{ \'glyphicon-pause\': isPlaying(song), \'glyphicon-play\': !isPlaying(song) }"></span>' +
-                    '</button>' +
-                  '</td>' +
-                  '<td>{{ song.name }}</td>' +
-                  '<td><span ng-repeat="artist in song.artists">{{ artist.name }}{{ $last ? "" : ", " }}</span></td>' +
-                  '<td>{{ song.genres.join(", ") }}</td>' +
-                '</tr>' +
-              '</tbody>' +
-            '</table>' +
-        '</div>',
+    templateUrl: './oneArtist.html',
       controller: 'ArtistCtrl'
   });
 });
